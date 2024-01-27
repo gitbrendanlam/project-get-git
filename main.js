a6fec0f357ce57e334d6fe5d378b16978ab8cdca
Ashley Hannigan <ashleyhannigan88@gmail.com>
2024-01-27 12:01:25 -0500

    chrome ext

---
+++ main.js
@@ -0,0 +1,29 @@
+// document.addEventListener('DOMContentLoaded', () => {
+//     const body = document.querySelector('body');
+//     const title = document.createElement('h1');
+//     title.innerText = "Get Git";
+//     body.appendChild(title);
+  
+//     const codeButton = document.getElementById(':r5:');
+//     codeButton.style.color = "white";
+    
+//   })
+document.addEventListener('DOMContentLoaded', () => {
+    const body = document.querySelector('body');
+    const title = document.createElement('h1');
+    title.innerText = "Get Git";
+    body.appendChild(title);
+  
+    document.querySelector("#responsive-meta-container > div > div:nth-child(2)").style.color = "red"
+    // fetch('https://github.com/gitbrendanlam/project-get-git/tree/main')
+    //   .then( response => response.text())
+    //   .then(response => {console.log(response)});
+      // .then( (responseText) => {
+      //   const doc = new DOMParser().parseFromString(responseText, 'text/html');
+      //   const codeButton = doc.getElementById(':r5:');
+      //   codeButton.style.color = 'white';
+      //   console.log('accessed site');
+      // })
+    
+  })
+  
